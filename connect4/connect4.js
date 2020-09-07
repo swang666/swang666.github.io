b86687035800b0c658fe8ef669a040cf02cbf0ca
Sumeng Wang <swang666@g.ucla.edu>
2020-09-07 13:29:41 -0700

    add more logic to match vertical and diagonal 4

--- connect4/connect4.js
+++ connect4/connect4.js
@@ -18,8 +18,17 @@ function setup(){
         idx = findEmptySpace(idx)
         if (idx >= 0){
             fillPlayerColor(idx, playerColor)
-            checkHorizontalFour(idx, isplayer1)
-            isplayer1 = !isplayer1
+            if (checkHorizontalFour(idx, isplayer1)){
+                return true
+            }else if (checkVerticalFour(idx, isplayer1)){
+                return true
+            }else if (checkleftDiagFour(idx, isplayer1)){
+                return true
+            }else if (checkrightDiagFour(idx, isplayer1)){
+                return true
+            }else{
+                isplayer1 = !isplayer1
+            }
         }
     })
     $('button#rematch').on('click', function(){
@@ -59,7 +68,109 @@ function checkHorizontalFour(idx, isplayer1){
                 $('td#score2').text(score2)
             }
             buttons.off()
+            return true
         }
+    }
+    return false
+}
+
+function checkVerticalFour(idx, isplayer1){
+    var start = idx % 7
+    for (var i = 0; i < 3; i++){
+        if (buttons.eq(start + i * 7).css('background-color') == buttons.eq(start + (i+1) * 7).css('background-color') && buttons.eq(start + (i+1) * 7).css('background-color') == buttons.eq(start + (i+2) * 7).css('background-color') && buttons.eq(start + (i+2) * 7).css('background-color') == buttons.eq(start + (i+3) * 7).css('background-color') && buttons.eq(start + i * 7).css('background-color') != "rgb(239, 239, 239)"){
+            if (isplayer1){
+                $('h2#result').text(player1 + " won!")
+                score1 += 1
+                $('td#score1').text(score1)
+            }else{
+                $('h2#result').text(player2 + " won!")
+                score2 += 1
+                $('td#score2').text(score2)
+            }
+            buttons.off()
+            return true
+        }
+    }
+    return false
+}
+
+function checkleftDiagFour(idx, isplayer1){
+    var invalidPos = [21, 28, 35, 29, 36, 37, 4,5,6,12,13,20]
+    if (!invalidPos.includes(idx)){
+        var col = idx % 7
+        var row = Math.floor(idx / 7)
+        var leftcol = col
+        var leftrow = row
+        var steps = 3
+        while (leftcol > 0 && leftrow > 0 && steps > 0){
+            leftcol --
+            leftrow --
+            steps --
+        }
+        var rightcol = col
+        var rightrow = row
+        steps = 4
+        while (rightcol < 7 && rightrow < 6 && steps > 0){
+            rightcol ++
+            rightrow ++
+            steps --
+        }
+        for (var i = 0; i < rightcol - leftcol - 3; i++){
+            if (buttons.eq((leftrow+i)*7 + (leftcol + i)).css('background-color') == buttons.eq((leftrow+i+1)*7 + (leftcol + i+1)).css('background-color') && buttons.eq((leftrow+i+1)*7 + (leftcol + i+1)).css('background-color') == buttons.eq((leftrow+i+2)*7 + (leftcol + i+2)).css('background-color') && buttons.eq((leftrow+i+2)*7 + (leftcol + i+2)).css('background-color') == buttons.eq((leftrow+i+3)*7 + (leftcol + i+3)).css('background-color')){
+                if (isplayer1){
+                    $('h2#result').text(player1 + " won!")
+                    score1 += 1
+                    $('td#score1').text(score1)
+                }else{
+                    $('h2#result').text(player2 + " won!")
+                    score2 += 1
+                    $('td#score2').text(score2)
+                }
+                buttons.off()
+                return true
+            }
+        }
+        return false
+    }
+}
+
+function checkrightDiagFour(idx, isplayer1){
+    var invalidPos = [0,1,2,7,8,14,27,33,34,39,40,41]
+    if (!invalidPos.includes(idx)){
+        var col = idx % 7
+        var row = Math.floor(idx / 7)
+        var leftcol = col
+        var leftrow = row
+        var steps = 3
+        while (leftcol > 0 && leftrow < 5 && steps > 0){
+            leftcol --
+            leftrow ++
+            steps --
+        }
+        var rightcol = col
+        var rightrow = row
+        steps = 4
+        while (rightcol < 7 && rightrow >= 0 && steps > 0){
+            rightcol ++
+            rightrow --
+            steps --
+        }
+        for (var i = 0; i < rightcol - leftcol - 3; i++){
+            if (buttons.eq((leftrow-i)*7 + (leftcol + i)).css('background-color') == buttons.eq((leftrow-i-1)*7 + (leftcol + i+1)).css('background-color') && buttons.eq((leftrow-i-1)*7 + (leftcol + i+1)).css('background-color') == buttons.eq((leftrow-i-2)*7 + (leftcol + i+2)).css('background-color') && buttons.eq((leftrow-i-2)*7 + (leftcol + i+2)).css('background-color') == buttons.eq((leftrow-i-3)*7 + (leftcol + i+3)).css('background-color')){
+                if (isplayer1){
+                    $('h2#result').text(player1 + " won!")
+                    score1 += 1
+                    $('td#score1').text(score1)
+                }else{
+                    $('h2#result').text(player2 + " won!")
+                    score2 += 1
+                    $('td#score2').text(score2)
+                }
+                buttons.off()
+                return true
+            }
+        }
+        return false
     }
 }
 
